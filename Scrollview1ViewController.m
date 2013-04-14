a55b32939d3c5baafa998342c6ebf5b61f9a9746
Agathe Battestini <agathe@morseproject.com>
2013-04-13 21:54:09 -0700

    git testing

--- Scrollview1ViewController.m
+++ Scrollview1ViewController.m
@@ -84,11 +84,14 @@
     [self.contentView addSubview:view2];
     view2.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-8-[view2]-8-|" options:0 metrics:nil views:@{@"view2": view2}]];
+    
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view2]-8-|" options:0 metrics:nil views:@{@"view2": view2}]];
-    [view2 setContentHuggingPriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisVertical];
+    
+//    [view2 setContentHuggingPriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisVertical]; // commenting this line makes the layout ambiguous
     
     // space out the subviews, which makes the contentview grow vertically
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view1]-400-[view2]" options:0 metrics:nil views:@{@"view2": view2, @"view1": view1}]];
+    view1.con
 
     /**
      no ambiguous layout
