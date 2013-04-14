2dd69ae59a35cf95ab489c8a49d9c197d50367b9
Agathe Battestini <agathe@morseproject.com>
2013-04-13 19:52:05 -0700

    fixed ambiguous layout

--- Scrollview1ViewController.m
+++ Scrollview1ViewController.m
@@ -85,9 +85,10 @@
     view2.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-8-[view2]-8-|" options:0 metrics:nil views:@{@"view2": view2}]];
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view2]-8-|" options:0 metrics:nil views:@{@"view2": view2}]];
+    [view2 setContentHuggingPriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisVertical];
     
     // space out the subviews, which makes the contentview grow vertically
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view1]-600-[view2]" options:0 metrics:nil views:@{@"view2": view2, @"view1": view1}]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[view1]-400-[view2]" options:0 metrics:nil views:@{@"view2": view2, @"view1": view1}]];
 
     /**
      no ambiguous layout
